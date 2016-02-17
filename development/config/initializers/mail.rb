a1d9e4c0099f1c39e95b375f9a0405a0a284921a
“wllamb” <wllamb@valdosta.edu>
2016-02-17 10:45:30 -0500

    Fixed user settings to allow email confirmation

--- development/config/initializers/mail.rb
+++ development/config/initializers/mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "gazeboapp",
-  :password             => "null",
+  :password             => "Gazeboiscool!",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
