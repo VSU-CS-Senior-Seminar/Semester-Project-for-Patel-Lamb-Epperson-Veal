42397d4181283873b3d3e6ed96c4309988cf2c6e
crveal <crveal@valdosta.edu>
2016-03-25 16:10:18 -0400

    added mailboxer

--- development/app/controllers/application_controller.rb
+++ development/app/controllers/application_controller.rb
@@ -8,5 +8,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 protect_from_forgery with: :exception
+helper_method :mailbox
 
+  private
+
+  def mailbox
+    @mailbox ||= current_user.mailbox
+  end
 end
