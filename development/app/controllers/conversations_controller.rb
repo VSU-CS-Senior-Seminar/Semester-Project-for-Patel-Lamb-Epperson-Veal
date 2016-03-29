ffe6d3f34aed891cf3a2fc56b73faae53321a4f5
crveal <crveal@valdosta.edu>
2016-03-29 17:16:01 -0400

    Message system implemented with styling

--- development/app/controllers/conversations_controller.rb
+++ development/app/controllers/conversations_controller.rb
@@ -7,9 +7,16 @@ class ConversationsController < ApplicationController
   def create
     recipients = User.where(id: conversation_params[:recipients])
     conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
-    flash[:success] = "Your message was successfully sent!"
+    flash[:notice] = "Your message was successfully sent!"
     redirect_to conversation_path(conversation)
   end
+
+  def show
+    @receipts = conversation.receipts_for(current_user).order("created_at ASC")
+    # mark conversation as read
+    conversation.mark_as_read(current_user)
+  end
+
 
   def reply
     current_user.reply_to_conversation(conversation, message_params[:body])
@@ -17,10 +24,14 @@ class ConversationsController < ApplicationController
     redirect_to conversation_path(conversation)
   end
 
-  def show
-    @receipts = conversation.receipts_for(current_user)
-    # mark conversation as read
-    conversation.mark_as_read(current_user)
+  def trash
+    conversation.move_to_trash(current_user)
+    redirect_to mailbox_inbox_path
+  end
+
+  def untrash
+    conversation.untrash(current_user)
+    redirect_to mailbox_inbox_path
   end
 
 
