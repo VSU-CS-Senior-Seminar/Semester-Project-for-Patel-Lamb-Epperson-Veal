8e9646350150a131e6de594163087dd198fafe29
crveal <crveal@valdosta.edu>
2016-04-09 17:10:58 -0400

    User can alterante between up and down vote

--- development/app/controllers/reputations_controller.rb
+++ development/app/controllers/reputations_controller.rb
@@ -2,6 +2,11 @@ class ReputationsController < ApplicationController
 
   def upvote
     user = User.find(params[:id])
+    if (Posts_Disliked.all.where(:post_id => params[:pid], :user_id => user.id).count(:all) == 1)
+      Posts_Disliked.where(:post_id => params[:pid], :user_id => user.id).destroy_all
+      user.update_attribute(:reputation, user.reputation+1)
+    else
+    end
     likedpost = Posts_Liked.new
     likedpost.post_id = params[:pid]
     likedpost.user_id = user.id
@@ -17,6 +22,11 @@ class ReputationsController < ApplicationController
 
   def downvote
     user = User.find(params[:id])
+    if (Posts_Liked.all.where(:post_id => params[:pid], :user_id => user.id).count(:all) == 1)
+      Posts_Liked.where(:post_id => params[:pid], :user_id => user.id).destroy_all
+      user.update_attribute(:reputation, user.reputation-1)
+    else
+    end
     dislikedpost = Posts_Disliked.new
     dislikedpost.post_id = params[:pid]
     dislikedpost.user_id = user.id
