198dc9117a0d9d4ab2195feadc4bf85fb882c8a1
harrisonepperson <harrisonepperson@gmail.com>
2016-04-19 18:11:59 -0400

    Changed events forum to Business Forum

--- development/app/helpers/neighborhood_helper.rb
+++ development/app/helpers/neighborhood_helper.rb
@@ -9,25 +9,25 @@ def createSubForums(newForum)
 
   newSubTwo = Forem::Forum.new
   newSubTwo.title = "Lost"
-  newSubTwo.description = "Lost something? Post about it here!"
+  newSubTwo.description = "Lost something? Post about it here."
   newSubTwo.category_id = newForum.id
   newSubTwo.save
 
   newSubThree = Forem::Forum.new
   newSubThree.title = "Found"
-  newSubThree.description = "Found something? Post about it here!"
+  newSubThree.description = "Found something? Post about it here."
   newSubThree.category_id = newForum.id
   newSubThree.save
 
   newSubFour = Forem::Forum.new
   newSubFour.title = "For Sale"
-  newSubFour.description = "Sell items to your community here!"
+  newSubFour.description = "Sell items to your community here."
   newSubFour.category_id = newForum.id
   newSubFour.save
 
   newSubFive = Forem::Forum.new
-  newSubFive.title = "Events"
-  newSubFive.description = "Post about any upcoming community events here."
+  newSubFive.title = "Business"
+    newSubFive.description = "Talk about local businesses here."
   newSubFive.category_id = newForum.id
   newSubFive.save
 end
