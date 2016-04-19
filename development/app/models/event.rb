dc18a52ff52dcf135e9d4a2c0db6f417aece6577
harrisonepperson <harrisonepperson@gmail.com>
2016-04-19 03:23:56 -0400

    migrated User to events table, added user value on event creation.

--- development/app/models/event.rb
+++ development/app/models/event.rb
@@ -1,2 +1,7 @@
 class Event < ActiveRecord::Base
+	belongs_to :user, :foreign_key => :user_id, class_name: 'User'
+	
+	def neighborhood_id
+		user.neighborhood_id
+	end
 end
--- development/db/schema.rb
+++ development/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414204009) do
+ActiveRecord::Schema.define(version: 20160419055329) do
 
   create_table "chats", force: :cascade do |t|
     t.integer  "sender_id"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160414204009) do
     t.datetime "end_time"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "forem_categories", force: :cascade do |t|
